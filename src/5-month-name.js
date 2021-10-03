6568e0317494e885713f2db71646ee9883f0ad04
Jakob Lindström <Snake@Jakobs-MacBook-Pro-2.local>
2021-10-03 11:50:25 +0200

    unit testing labs done

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,22 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+  if (monthNumber < 13 && monthNumber > 0) {
+    return months[monthNumber - 1]
+  } else {
+    return null
+  }
 }
